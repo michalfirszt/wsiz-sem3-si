13261450efaaaa502555fb1993a4e6ee2342cfc4
michalfirszt <michalfiszu@gmail.com>
2024-02-03 11:15:02 +0100

    Remove elements select

--- 08/js/index.js
+++ 08/js/index.js
@@ -7,11 +7,27 @@ const getData = async (url, method, onSuccess) => {
 
     if (response.status === 200) {
         onSuccess(data)
+    } else {
+        console.erorr('Bad request')
     }
 }
 
 const select = document.getElementById('select-input');
 
 getData('http://localhost:3000/posts', 'GET', (data) => {
-    console.log(data);
+    data.forEach((post) => {
+        const option = document.createElement('option')
+        option.value = post.id;
+        option.textContent = post.title;
+
+        select.appendChild(option);
+    })
+})
+
+const removeButton = document.getElementById('remove-button')
+
+removeButton.addEventListener('click', () => {
+    getData(`http://localhost:3000/posts/${select.value}`, 'DELETE', () => {
+        select.remove(select.selectedIndex)
+    })
 })
